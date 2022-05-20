785d6175f7d8d34a4252a6bcb87f60370510c7a3
Ming Sun <ming.sun@digital.hmrc.gov.uk>
2022-05-20 17:18:43 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Datastream dummy app listing on port 8100
+
+```
+npm start
+```
---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const bodyparser = require('body-parser')
+const app = express()
+const port = 8100
+
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({ extended: true }))
+
+// accept POST requests on the / endpoint
+app.post('/write/audit', (req, res) => {
+    // show request json data
+    console.log(req.body);
+    // send OK response
+    res.send('OK');
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Datastream dummy listening on port ${port}`)
+})
+
